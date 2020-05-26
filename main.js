ad5971ea918c8f811f3f1e3d8bab45bc07018f5b
Frakir59 <frakir.fps.tv@gmail.com>
2020-05-26 16:56:49 +0200

    Création config + masque du token

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const {Client} = require('discord.js');
-const client = new Client({disableEveryone: true});
+const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config.js");
+const client = new Client({ disableEveryone: true });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log("Je suis prêt");
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') msg.reply('Pong!');
-  if (msg.content === 'everyone') msg.channel.send("@everyone, salut à tous !", {disableEveryone: false});
-  if (msg.content === 'noteveryone') msg.channel.send("@everyone (noteveryone), Salut à tous !", {disableEveryone: false});
+client.on("message", msg => {
+  // Convertion de everyone et noteveryone en @everyone et @noteveryone avec le message salut a tous
+  if (msg.content === "everyone") msg.channel.send("@everyone, salut à tous !", { disableEveryone: false });
+  if (msg.content === "noteveryone") msg.channel.send("@everyone (noteveryone), Salut à tous !", { disableEveryone: false });
 });
 
-client.login('NzE0NzkzOTE5MTY2MDIxNjkz.Xsz16g.6BFSybEGhnSwAY-VFnqt4c8IT5U');
+client.login(TOKEN);
