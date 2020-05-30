893e51d2ac773e17a1729a41c8500235e5553ae4
Frakir59 <frakir.fps.tv@gmail.com>
2020-05-30 14:10:35 +0200

    Création de la class guildMemberAdd

--- main.js
+++ main.js
@@ -10,24 +10,21 @@ client.on("debug", console.log);
 
 // Arrivé d'un nouveau membre sur discord -> envois d'un message privé + msg dans les-nouveaux
 client.on("guildMemberAdd", member => {
-  // Envois du message privé
-  member.send(`
-  Bonjour et bienvenue **` + member.displayName + `** sur le serveur discord de **Frakir** !
-N'oublie pas d'accepter le  **📝Réglement** pour profiter entièrement du seveur !`);
-  // On cherche le channel les-nouveaux
-  client.channels.cache.get("714864843412340766").send(`${member} a rejoint le serveur !`);
+    const { guildMemberAdd } = require("./Utilisateur/MemberAdd/guildMemberAdd.js");
+    const guildMemberadd = new guildMemberAdd(client, member);
+    guildMemberadd.MessageGuildMemberAdd();
 });
 
 client.on("message", msg => {
   // On bloque les commandes à boucle infine
   if (msg.author.bot) return;
   // Création des commandes
-  // Création d'un tableau des arguments saisies apres cmd/
+  // Création d'un tableau des arguments
   const arrayArgs = msg.content.split(/ +/g);
-  // On retire cmd/ des arguments récupérés
+  // On retire le début de la commande des arguments récupérés
   const cmd = arrayArgs.shift().toLowerCase();
   console.log("Création du tableau de commandes...: cmd/");
-  // On execute les commandes cmd/
+  // On execute les commandes
   if (cmd === `${PREFIX}test`) msg.channel.send("ok");
   // Convertion de everyone et noteveryone en @everyone et @noteveryone avec le message salut a tous
   if (msg.content === "everyone") msg.channel.send("@everyone, salut à tous !", { disableEveryone: false });
